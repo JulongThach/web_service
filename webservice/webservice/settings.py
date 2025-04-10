1fabda7f245241e86a770552d472df096b6b95c6
Julong Thach <thachjulong@caisedu.com>
2025-04-10 10:26:24 +0700

    Update Staticfile

--- webservice/webservice/settings.py
+++ webservice/webservice/settings.py
@@ -118,13 +118,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = '/static/'
-MEDIA_URL  = '/images/'
-STATIC_ROOT  = os.path.join(BASE_DIR, 'staticfiles')
 
-# Static & Template Setup
+
+STATIC_URL = 'static/'
+MEDIA_URL  = '/images/'
+STATIC_ROOT  = 'staticfiles'
+
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    os.path.join(BASE_DIR, 'static')
 ]
 
 TEMPLATES[0]['DIRS'] = [BASE_DIR / 'steahouse/templates']
