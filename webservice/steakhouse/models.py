c996755cd2e3c9dd8658b6b9b15421265e585786
Julong Thach <thachjulong@caisedu.com>
2025-04-11 17:06:25 +0700

    Update

--- webservice/steakhouse/models.py
+++ webservice/steakhouse/models.py
@@ -26,7 +26,7 @@ class Order(models.Model):
     receipt_number = models.CharField(max_length=100, unique=True, editable=False)
     customer_name = models.CharField(max_length=255)
     phone_number = models.CharField(max_length=20)
-    address = models.TextField()
+    address = models.TextField(null=True, blank=True)
     latitude = models.DecimalField(max_digits=9, decimal_places=6, null=True, blank=True)
     longitude = models.DecimalField(max_digits=9, decimal_places=6, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
